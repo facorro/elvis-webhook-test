bbcf937dea9bef1913a059ce94699faeb33b237c
Juan Facorro <juan.facorro@gmail.com>
2014-07-04 10:51:19 -0300

    Some other changes.

--- src/dummy.erl
+++ src/dummy.erl
@@ -1,1 +1,16 @@
 -module(dummy).
+
+-export([
+         dummy/0,
+         derp/0,
+         dumb/0
+        ]).
+
+dummy() ->
+    io:format("dummy").
+
+derp() ->
+    io:format("derp").
+
+dumb() ->
+    io:format("dumb").
